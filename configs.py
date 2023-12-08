ff9fe3344f095e4661b2291867e88b44e16686a3
nivas69 <115863263+nivas69@users.noreply.github.com>
2023-12-08 13:54:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config:
     API_HASH = getenv("API_HASH", "67bd6a23278a884e575a6ebf15b0e6bc")
     BOT_TOKEN = getenv("BOT_TOKEN", "6664635995:AAFS1suOi1wQb8CyoiwcNWvQB6D3RBZmRs4")
     FSUB = getenv("FSUB", "NMOAdda")
-    CHID = int(getenv("CHID", "--1001732268000"))
+    CHID = int(getenv("CHID", "-1001732268000"))
     SUDO = list(map(int, getenv("SUDO", "6402835574").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://nivasshetty46:AmuQSnZPzWzbPIBS@cluster0.kz11jeq.mongodb.net/?retryWrites=true&w=majority")
     
