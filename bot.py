3a58eae22fc2c3c32e9d1516f0566b4920afa6c2
nivas69 <115863263+nivas69@users.noreply.github.com>
2023-12-08 14:39:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @NivasBots
+# Subscribe YouTube Channel For Amazing Bot @NivasBots
+# Ask Doubt on telegram @NivasBots
 
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram import filters, Client, errors, enums
@@ -42,7 +42,7 @@ async def approve(_, m : Message):
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
-        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @NMOAdda__**".format(m.from_user.mention, m.chat.title))
+        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @NivasBots__**".format(m.from_user.mention, m.chat.title))
         add_user(kk.id)
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
@@ -60,9 +60,9 @@ async def op(_, m :Message):
                 [
                     [
                         InlineKeyboardButton("🗯 Channel", url="https://t.me/NMOAdda"),
-                        InlineKeyboardButton("💬 Support", url="https://t.me/NMOAdda")
+                        InlineKeyboardButton("💬 Support", url="https://t.me/NivasBots")
                     ],[
-                        InlineKeyboardButton("➕ Add me to your Chat ➕", url="http://t.me/JoinRequestApporve_Bot")
+                        InlineKeyboardButton("➕ Add me to your Chat ➕", url="https://t.me/NivasBots")
                     ]
                 ]
             )
@@ -73,7 +73,7 @@ async def op(_, m :Message):
             keyboar = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("💁‍♂️ Start me private 💁‍♂️", url="http://t.me/NMOAdda")
+                        InlineKeyboardButton("💁‍♂️ Start me private 💁‍♂️", url="https://t.me/NivasBots")
                     ]
                 ]
             )
@@ -102,9 +102,9 @@ async def chk(_, cb : CallbackQuery):
                 [
                     [
                         InlineKeyboardButton("🗯 Channel", url="https://t.me/NMOAdda"),
-                        InlineKeyboardButton("💬 Support", url="https://t.me/NMOAdda")
+                        InlineKeyboardButton("💬 Support", url="https://t.me/NivasBots")
                     ],[
-                        InlineKeyboardButton("➕ Add me to your Chat ➕", url="http://t.me/NMOAdda")
+                        InlineKeyboardButton("➕ Add me to your Chat ➕", url="https://t.me/NivasBots")
                     ]
                 ]
             )
